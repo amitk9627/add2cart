cf4f13df7a7b6a617e730fb4d335eb88941df296
Amit kumar <ramitgzp56@gmail.com>
2023-06-06 09:21:44 +0530

    bhhhdhd

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React, { Fragment } from 'react'
-import Home from './Pages/Home';
+
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Contact from './Pages/Contact';
-import Products from './Pages/Products/Products';
+import Navbar from './component/Navbar';
+
+
 
 
 const App = () => {
 
   return (
-   
-      <Fragment>
-        <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/link' />
-          <Route path='/products' element={<Products />}/>
-          <Route path='/contact' element={<Contact />}/>
-          <Route path='*' element={<div className='text-center text-danger'>page not found</div>}/>
-        </Routes>
-
-      </Fragment>
-      
+  
+     
+     <Navbar />
+    
+    
   )
 }
 
